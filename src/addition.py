04c7d702f414260240d61595065147b127656f0a
Vinyas LS <111056825+vinyasls@users.noreply.github.com>
2025-04-13 15:00:58 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,8 @@
 # app.py
 # This is a test commit
-def add(a, b):
-    return a + b
+def add(a, b, c):
+    return a + b + c
 
 def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(1, 2, 3) == 6
+    assert add(1, -1, 1) == 1
